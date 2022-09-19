298ef76963784005816a2ff4f29e1e14a23d7ab9
Pedro Reynoso <113930313+preynoso3@users.noreply.github.com>
2022-09-19 17:49:05 +0000

    making list a sentence

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ print(len(newList2))
 #####################################challenge 2#######################################################
 # Join the following list into a string, separating each item with a space. Use the appropriate list/string method, and display the result.
 word_list = ["Simple", "is", "better", "than", "complex."]
-
+print(" ".join(word_list))
 # Print the following text in uppercase, using the specific string method:
 
 text1 = "Especially in electronic communications, writing in all caps is equivalent to yelling."
